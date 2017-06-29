9109d05cd8c32a8733db1586e74a29c763709626
Jim Bailey <jbailey@supramed.com>
2017-06-29 16:49:30 -0700

    Initial commit

---
+++ src/ToDoListAngular/app/scripts/app.js
@@ -0,0 +1,56 @@
+﻿//
+// Use the following code for running without authentication.
+//
+//'use strict';
+//angular.module('todoApp', ['ngRoute'])
+//.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+//    $routeProvider.when("/Home", {
+//        controller: "homeCtrl",
+//        templateUrl: "/App/Views/Home.html",
+//    }).when("/TodoList", {
+//        controller: "todoListCtrl",
+//        templateUrl: "/App/Views/TodoList.html",
+//    }).when("/UserData", {
+//        controller: "userDataCtrl",
+//        templateUrl: "/App/Views/UserData.html",
+//    }).otherwise({ redirectTo: "/Home" });
+//}]);
+
+
+//
+// Use the following code for running with authentication.
+//
+'use strict';
+angular.module('todoApp', ['ngRoute', 'AdalAngular'])
+.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
+
+    $routeProvider.when("/Home", {
+        controller: "homeCtrl",
+        templateUrl: "/App/Views/Home.html",
+    }).when("/TodoList", {
+        controller: "todoListCtrl",
+        templateUrl: "/App/Views/TodoList.html",
+        requireADLogin: true,
+    }).when("/UserData", {
+        controller: "userDataCtrl",
+        templateUrl: "/App/Views/UserData.html",
+    }).otherwise({ redirectTo: "/Home" });
+
+    var endpoints = {
+        "https://todolistapi20170629.azurewebsites.net/": "e1d247d9-3ad8-4445-a737-35fc4374a8a3"
+    };
+
+    adalProvider.init(
+        {
+            instance: 'https://login.microsoftonline.com/',
+            tenant: 'jimbtuneshotmail.onmicrosoft.com',
+            clientId: 'e1d247d9-3ad8-4445-a737-35fc4374a8a3',
+            extraQueryParameter: 'nux=1',
+            endpoints: endpoints
+            //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+        },
+        $httpProvider
+        );
+
+}]);
+
